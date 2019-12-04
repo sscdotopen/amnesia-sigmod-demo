91271d60a53f6ff754b3b8168890504174e5f405
sscdotopen <ssc.open@googlemail.com>
2019-12-04 09:02:44 -0500

    hacky working version

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,14 @@ use timely::communication::allocator::thread::Thread;
 use timely::worker::Worker;
 
 use differential_dataflow::input::InputSession;
-use differential_dataflow::operators::CountTotal;
 
 use ws::listen;
 
 use amnesia_sigmod_demo::server::Server;
-
+use differential_dataflow::operators::arrange::ArrangeByKey;
+use timely::dataflow::operators::Probe;
+use differential_dataflow::operators::join::JoinCore;
+use differential_dataflow::operators::{CountTotal,Count};
 
 fn main() {
     let alloc = Thread::new();
@@ -23,25 +25,78 @@ fn main() {
     demo(Arc::clone(&worker));
 
     while worker.borrow_mut().step_or_park(None) { }
-
 }
 
 fn demo(worker: Arc<RefCell<Worker<Thread>>>) {
 
     let mut interactions_input: InputSession<usize, (u32, u32), isize> = InputSession::new();
 
-    let the_probe = worker.borrow_mut().dataflow(|scope| {
+    let mut the_probe = timely::dataflow::operators::probe::Handle::new();
+
+    let (num_interactions_per_item_trace, cooccurrences_trace,
+        jaccard_similarities_trace)= worker.borrow_mut().dataflow(|scope| {
         let interactions = interactions_input.to_collection(scope);
 
-        interactions
+        let interactions = interactions_input.to_collection(scope);
+
+        let num_interactions_per_item = interactions
             .map(|(_user, item)| item)
-            .count_total()
-            .inspect(|x| println!("{:?}", x))
-            .probe()
+            .count_total();
+
+        let arranged_num_iteractions_per_item = num_interactions_per_item.arrange_by_key();
+
+        let arranged_remaining_interactions = interactions.arrange_by_key();
+
+        // Compute the number of cooccurrences of each item pair
+        let cooccurrences = arranged_remaining_interactions
+            .join_core(&arranged_remaining_interactions, |_user, &item_a, &item_b| {
+                if item_a > item_b { Some((item_a, item_b)) } else { None }
+            })
+            .count();
+
+        let arranged_cooccurrences = cooccurrences.arrange_by_key();
+
+        let arranged_num_interactions_per_item = num_interactions_per_item.arrange_by_key();
+
+        // Compute the jaccard similarity between item pairs (= number of users that
+        // interacted with both items / number of users that interacted with at least
+        // one of the items)
+        let jaccard_similarities = cooccurrences
+            // Find the number of interactions for item_a
+            .map(|((item_a, item_b), num_cooc)| (item_a, (item_b, num_cooc)))
+            .join_core(
+                &arranged_num_interactions_per_item,
+                |&item_a, &(item_b, num_cooc), &occ_a| {
+                    Some((item_b, (item_a, num_cooc, occ_a)))
+                }
+            )
+            // Find the number of interactions for item_b
+            .join_core(
+                &arranged_num_interactions_per_item,
+                |&item_b, &(item_a, num_cooc, occ_a), &occ_b| {
+                    Some(((item_a, item_b), (num_cooc, occ_a, occ_b)))
+                },
+            )
+            // Compute Jaccard similarty, has to be done in a map due to the lack of a
+            // total order for f64 (which seems to break the consolidation in join)
+            .map(|((item_a, item_b), (num_cooc, occ_a, occ_b))| {
+                let jaccard = num_cooc as f64 / (occ_a + occ_b - num_cooc) as f64;
+                ((item_a, item_b), jaccard.to_string())
+            });
+
+        let arranged_jaccard_similarities = jaccard_similarities.arrange_by_key();
+
+        arranged_jaccard_similarities.stream.probe_with(&mut the_probe);
+
+        (arranged_num_interactions_per_item.trace, arranged_cooccurrences.trace,
+            arranged_jaccard_similarities.trace)
     });
 
     let input = Arc::new(RefCell::new(interactions_input));
     let probe = Arc::new(RefCell::new(the_probe));
+    let shared_num_interactions_per_item_trace = Arc::new(RefCell::new(num_interactions_per_item_trace));
+    let shared_cooccurrences_trace = Arc::new(RefCell::new(cooccurrences_trace));
+    let shared_similarities_trace = Arc::new(RefCell::new(jaccard_similarities_trace));
 
     // Listen on an address and call the closure for each connection
     listen("127.0.0.1:8000", |out| {
@@ -50,7 +105,10 @@ fn demo(worker: Arc<RefCell<Worker<Thread>>>) {
             out,
             worker: Arc::clone(&worker),
             input: Arc::clone(&input),
-            probe: Arc::clone(&probe)
+            probe: Arc::clone(&probe),
+            shared_num_interactions_per_item_trace: Arc::clone(&shared_num_interactions_per_item_trace),
+            shared_cooccurrences_trace: Arc::clone(&shared_cooccurrences_trace),
+            shared_similarities_trace: Arc::clone(&shared_similarities_trace)
         }
     }).unwrap();
 }
--- src/server.rs
+++ src/server.rs
@@ -5,16 +5,21 @@ use timely::dataflow::ProbeHandle;
 
 use differential_dataflow::input::InputSession;
 use ws::{Handler, Message, Request, Response, Result, Sender};
-
+use differential_dataflow::operators::arrange::TraceAgent;
+use differential_dataflow::trace::implementations::spine_fueled::Spine;
+use differential_dataflow::trace::implementations::ord::OrdValBatch;
 use timely::communication::allocator::thread::Thread;
 use timely::worker::Worker;
+use differential_dataflow::trace::{Cursor, TraceReader};
 
 use std::fs::File;
 use std::io::Read;
 
 use std::cell::RefCell;
 use std::sync::Arc;
+use std::rc::Rc;
 
+use serde_json::json;
 use serde_json::Result as SerdeResult;
 
 use crate::requests::{Change, ChangeRequest};
@@ -24,7 +29,10 @@ pub struct Server {
     pub out: Sender,
     pub worker: Arc<RefCell<Worker<Thread>>>,
     pub input: Arc<RefCell<InputSession<usize, (u32, u32), isize>>>,
-    pub probe: Arc<RefCell<ProbeHandle<usize>>>
+    pub probe: Arc<RefCell<ProbeHandle<usize>>>,
+    pub shared_num_interactions_per_item_trace: Arc<RefCell<TraceAgent<Spine<u32, isize, usize, isize, Rc<OrdValBatch<u32, isize, usize, isize>>>>>>,
+    pub shared_cooccurrences_trace: Arc<RefCell<TraceAgent<Spine<(u32, u32), isize, usize, isize, Rc<OrdValBatch<(u32, u32), isize, usize, isize>>>>>>,
+    pub shared_similarities_trace: Arc<RefCell<TraceAgent<Spine<(u32, u32), String, usize, isize, Rc<OrdValBatch<(u32, u32), String, usize, isize>>>>>>,
 }
 
 fn read_index_html() -> Vec<u8> {
@@ -69,16 +77,91 @@ impl Handler for Server {
                     self.probe.borrow_mut().less_than(interactions_input.time())
                 });
 
+                //TODO collect changes from traces...
+
                 println!("{:?}", request);
+
+                //let (mut cursor, storage) = self.trace.borrow_mut().cursor();
+
+                let num_interactions_per_item_messages = collect_diffs(
+                    Arc::clone(&self.shared_num_interactions_per_item_trace),
+                    self.current_step - 1,
+                    |key, value, time, change| {
+
+                        let json = json!({
+                            "data": "item_interactions_n",
+                            "item": key,
+                            "count": value,
+                            "time": time,
+                            "change": change
+                        });
+
+                        Message::text(json.to_string())
+                    });
+
+
+                for message in num_interactions_per_item_messages.iter() {
+                    // TODO remove clone maybe with drain or so
+                    self.out.broadcast(message.clone()).expect("Cannot send message");
+                }
+
+                let cooccurrence_messages = collect_diffs(
+                    Arc::clone(&self.shared_cooccurrences_trace),
+                    self.current_step - 1,
+                    |key, value, time, change| {
+
+                        let (item_a, item_b) = key;
+
+                        let json = json!({
+                            "data": "cooccurrences_c",
+                            "item_a": item_a,
+                            "item_b": item_b,
+                            "num_cooccurrences": value,
+                            "time": time,
+                            "change": change
+                        });
+
+                        Message::text(json.to_string())
+                    });
+
+
+                for message in cooccurrence_messages.iter() {
+                    // TODO remove clone maybe with drain or so
+                    self.out.broadcast(message.clone()).expect("Cannot send message");
+                }
+
+                let similarities_messages = collect_diffs(
+                    Arc::clone(&self.shared_similarities_trace),
+                    self.current_step - 1,
+                    |key, value, time, change| {
+
+                        let (item_a, item_b) = key;
+
+                        let json = json!({
+                            "data": "similarities_s",
+                            "item_a": item_a,
+                            "item_b": item_b,
+                            "similarity": value.parse::<f64>().unwrap(),
+                            "time": time,
+                            "change": change
+                        });
+
+                        Message::text(json.to_string())
+                    });
+
+
+                for message in similarities_messages.iter() {
+                    // TODO remove clone maybe with drain or so
+                    self.out.broadcast(message.clone()).expect("Cannot send message");
+                }
+
             },
             Err(_) => println!("Error parsing request..."),
         }
 
-        // Broadcast to all connections
-        self.out.broadcast(Message::text("huhu"))
+        Ok(())
     }
 
-    //
     fn on_request(&mut self, req: &Request) -> Result<(Response)> {
         match req.resource() {
             "/ws" => Response::from_request(req),
@@ -86,4 +169,41 @@ impl Handler for Server {
             _ => Ok(Response::new(404, "Not Found", b"404 - Not Found".to_vec())),
         }
     }
+}
+
+use std::fmt::Debug;
+
+
+fn collect_diffs<K, V, F>(
+    trace: Arc<RefCell<TraceAgent<Spine<K, V, usize, isize, Rc<OrdValBatch<K, V, usize, isize>>>>>>,
+    time_of_interest: usize,
+    logic: F,
+) -> Vec<Message>
+    where V: Clone + Ord + Debug,
+          K: Clone + Ord + Debug,
+          F: Fn(&K, &V, usize, isize) -> Message +'static
+{
+    // TODO dont like it that we have to buffer them
+    let mut messages = Vec::new();
+
+    let (mut cursor, storage) = trace.borrow_mut().cursor();
+
+    while cursor.key_valid(&storage) {
+        while cursor.val_valid(&storage) {
+
+            let key = cursor.key(&storage);
+            let value = cursor.val(&storage);
+
+            cursor.map_times(&storage, |time, diff| {
+                if *time == time_of_interest {
+                    messages.push(logic(&key, &value, *time, *diff));
+                }
+            });
+
+            cursor.step_val(&storage);
+        }
+        cursor.step_key(&storage);
+    }
+
+    messages
 }
